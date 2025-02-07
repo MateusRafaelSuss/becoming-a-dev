ce1e1ec7c54970e63d263a94ea5033b7ce07e67a
MateusRafaelSuss <167236538+MateusRafaelSuss@users.noreply.github.com>
2025-02-07 09:58:59 -0300

    Update3 index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 echo "Hello, World";
 echo "Rafael Santos Borré irá marcar um hat-trick no gre-NAL";
-2+2 
-echo "testando o upload para o git hub"
+2+2;
+echo "testando o upload para o git hub";
+echo "teste o upload para diretorio local";
 ?>
