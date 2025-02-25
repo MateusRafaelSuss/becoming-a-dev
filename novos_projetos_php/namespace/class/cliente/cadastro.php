10e6957078134bd02f1b96806b2dd443a569c042
MateusRafaelSuss <mateussuss@gmail.com>
2025-02-25 11:03:54 -0300

    25-02-2025

---
+++ novos_projetos_php/namespace/class/cliente/cadastro.php
@@ -0,0 +1,10 @@
+<?php 
+class Cadastro extends \Cadastro{
+    public function registrarVenda(){
+        echo "A veda foi registrada para o cliente". $this -> getNome();
+    }
+}
+
+
+
+?>
---
+++ novos_projetos_php/namespace/index.php
@@ -0,0 +1,12 @@
+<?php 
+require_once("config.php");
+use cliente\Cadastro;
+
+$cad = new Cadastro();
+$cad -> setNome("Mateus Rafael ");
+$cad -> setEmail("mateussuss@gmail.com");
+$cad -> setSenha("123344");
+
+echo $cad;
+
+?> 
