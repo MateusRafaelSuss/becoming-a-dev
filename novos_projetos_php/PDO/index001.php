ae808f614ec33285a800a30df2f4dc7ee0c0b699
MateusRafaelSuss <mateussuss@gmail.com>
2025-02-28 11:02:12 -0300

    28-02-2025

---
+++ novos_projetos_php/PDO/index001.php
@@ -0,0 +1,20 @@
+<?php 
+///PDO
+$conn = new PDO("mysql:dbname=dbphp7;host=localhost", "root", "");//tipo do banco, nome do banco, servidor; usuario e senha 
+
+
+$stmt = $conn -> prepare("SELECT * FROM tb_usuarios");
+$stmt -> execute();
+$results = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+
+foreach($results as $row){
+    foreach($row as $key => $value){
+        echo "". $key;
+    }
+}
+
+var_dump($results);
+
+
+
+?> 
--- novos_projetos_php/mysqli/index001.php
+++ novos_projetos_php/mysqli/index001.php
@@ -3,17 +3,25 @@
 
 //o primeiro parametro é o endereço, o segundo é o usuario, o terceiro é a senha e o quarto o nome do banco de dados
 
-$connection = new mysqli("localhost", "root", "", "dbphp7");
-if($connection -> connect_error){
-    echo "Error". $connection -> connect_error;
+$conn = new mysqli("localhost", "root", "", "dbphp7");
+if($conn -> connect_error){
+    echo "Error". $conn -> connect_error;
 } //testa se a conexão está funcionando, e retorna a mensagem de erro se não estiver
 
 //manipular dados do banco pelo php
 
-$stmt = $connection -> prepare("INSERT INTO tb_usuarios(deslogin, desssenha) VALUES(? , ?);"); //as interrogações são os parametros que serão enviados ao banco de dados
+$stmt = $conn -> prepare("INSERT INTO tb_usuarios (deslogin, desssenha) VALUES(?, ?)"); //as interrogações são os parametros que serão enviados ao banco de dados
 $stmt -> bind_param("ss", $login, $password); //indica o tipo de dados(string string) que será inserido, e o dado
+
 $login = "user";
 $password = "12345";
+
+$stmt -> execute(); //manda esse statment (stmt) ser executado 
+///para adicionar mais dados é necessario apenaas repetir a atribuição e o execute
+
+$login = "Mateus";
+$senha = "22222";
+
 $stmt -> execute(); //manda esse statment (stmt) ser executado 
 
 
