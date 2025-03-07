c2ed3448bd4e3ac2baddb3e4d896a2af6a9d8008
MateusRafaelSuss <mateussuss@gmail.com>
2025-03-07 10:50:39 -0300

    07-03-2025

---
+++ novos_projetos_php/PDO/index005.php
@@ -0,0 +1,14 @@
+<?php 
+///transaactions 
+$conn = new PDO("mysql:host=localhost;dbname= dbphp7", "root", "");
+$conn -> beginTransaction();
+$stmt = $conn -> prepare("DELETE FROM tb_usuarios WHERE idusuario = ?");
+
+$id = 1;
+
+
+$stmt -> execute(array($id));
+
+//$conn -> rollBack()
+$conn -> commit();
+?>
