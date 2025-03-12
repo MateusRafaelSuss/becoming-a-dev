88538d33e3a107e65d1b515bc2ae9ac79d2e9515
MateusRafaelSuss <mateussuss@gmail.com>
2025-03-12 09:57:52 -0300

    PDO-DAO

--- novos_projetos_php/PDO-DAO/class/usuario.php
+++ novos_projetos_php/PDO-DAO/class/usuario.php
@@ -49,6 +49,37 @@ class usuario{
 
 }
 
+public static function getList(){
+    $sql = new Sql;
+    return $sql -> select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+}
+
+public static function search($login){
+    $sql = new Sql;
+    return $sql -> select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ? ORDER BY deslogin", array(
+        'SEARCH'=> "%". $login ."%"
+    ));
+
+}
+
+public function login($login, $password){
+    $sql = new Sql;
+        $result = $sql -> select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+            ":LOGIN"=> $login,
+            ":PASSWORD" => $password
+        ));
+
+        if (isset($result[0])){
+            $row = $result[0];
+            $this -> setIdusuario($row['idusuario']);
+            $this -> setDessenha($row['dessenha']);
+            $this -> setDtcadastro(new DateTime($row['dtcadastro']));
+        } else {
+            throw new Exception("Login ou senha invalidos ", 1);
+        }
+
+}
+
 public function __toString(){
     return json_encode(array(
         "idusuario" => $this -> getIdusuario,
--- novos_projetos_php/PDO-DAO/index.php
+++ novos_projetos_php/PDO-DAO/index.php
@@ -1,7 +1,21 @@
 <?php
 require_once("config.php");
+///Carrega um único usuário
 $root = new usuario();
 $root -> loadByid(3);
 echo $root;
+
+///Carrega uma lista de usuários
+$lista = usuario::getList();
+echo json_encode($lista); 
+
+///Carrega uma lista de usuarios buscando pelo login
+$search = usuario::search("jo");
+echo json_encode($search);
+
+///Carrega um usuario usando o login e a senha 
+$usuario = new usuario();
+$usuario -> login("", ""); 
+echo json_encode($usuario);
 
 ?> 
